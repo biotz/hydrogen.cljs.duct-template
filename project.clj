e07d5b1e719fc8420e3b97a6116b52737b5e0368
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-04-03 19:05:25 +0200

    Add deploy repositories configuration
    We are still not deploying automatically using TravisCI, but this sets
    up the required configuration to be able to do it.

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :repl-options {:init-ns hydrogen.cljs.duct-template})
