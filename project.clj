0a57e7fbebbc7711deb4e467b120b4f73d8eb379
Damian Hryniewicz <damian@magnet.coop>
2019-05-10 11:30:49 +0200

    Add cljfmt to project/dev plugins

--- project.clj
+++ project.clj
@@ -4,12 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+  :deploy-repositories [["snapshots" {:url      "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
                                       :sign-releases false}]
-                        ["releases"  {:url "https://clojars.org/repo"
+                        ["releases"  {:url      "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
                                       :sign-releases false}]]
-  :repl-options {:init-ns hydrogen.cljs.duct-template})
+  :repl-options {:init-ns hydrogen.cljs.duct-template}
+  :profiles
+  {:dev          [:project/dev :profiles/dev]
+   :profiles/dev {}
+   :project/dev  {:plugins [[lein-cljfmt "0.6.2"]]}})
